ae7180c7755e1512f047d976a1a5af12960f6b88
hexbabe <sean.xiaochen.yu@gmail.com>
2024-05-28 13:07:21 -0400

    Add deps apt install

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+sudo apt-get update
+sudo add-apt-repository universe &&
+sudo apt-get install -y libfuse2 ffmpeg pkg-config
+
 make module
 OS=$(uname -s)
 mv ./bin/$OS/module.tar.gz .
